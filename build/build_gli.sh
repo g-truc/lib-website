2c78e3577d418c908aae9f1f29c90a8c05a5a45c
Christophe Riccio <mail@g-truc.net>
2014-01-18 18:29:09 +0100

    Added GLI 0.5.1 release

--- build/build_gli.sh
+++ build/build_gli.sh
@@ -5,3 +5,6 @@ java -jar xalan.jar -OUT ../gli/0.4.1/code.html     -IN ../gli/source/data.xml  
 java -jar xalan.jar -OUT ../gli/0.5.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
 java -jar xalan.jar -OUT ../gli/0.5.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
 java -jar xalan.jar -OUT ../gli/0.5.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
+java -jar xalan.jar -OUT ../gli/0.5.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
+java -jar xalan.jar -OUT ../gli/0.5.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
+java -jar xalan.jar -OUT ../gli/0.5.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
