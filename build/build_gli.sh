c3f8f7c8bef4981c03352a3fbff8e07e9015d5c8
Christophe Riccio <christopheri@unity3d.com>
2015-09-01 01:51:49 +0200

    Preparing GLI 0.7.0 release

--- build/build_gli.sh
+++ build/build_gli.sh
@@ -1,16 +1,26 @@
 java -jar xalan.jar -OUT ../gli/gli.atom            -IN ../gli/source/data.xml   -XSL ./atom.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM SERVE atom
-java -jar xalan.jar -OUT ../gli/0.4.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
-java -jar xalan.jar -OUT ../gli/0.4.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
-java -jar xalan.jar -OUT ../gli/0.4.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
-java -jar xalan.jar -OUT ../gli/0.5.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
-java -jar xalan.jar -OUT ../gli/0.5.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
-java -jar xalan.jar -OUT ../gli/0.5.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
-java -jar xalan.jar -OUT ../gli/0.5.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
-java -jar xalan.jar -OUT ../gli/0.5.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
-java -jar xalan.jar -OUT ../gli/0.5.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
-java -jar xalan.jar -OUT ../gli/0.6.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
-java -jar xalan.jar -OUT ../gli/0.6.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
-java -jar xalan.jar -OUT ../gli/0.6.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
+
+java -jar xalan.jar -OUT ../gli/0.7.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.7.0'
+java -jar xalan.jar -OUT ../gli/0.7.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.7.0'
+java -jar xalan.jar -OUT ../gli/0.7.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.7.0'
+
 java -jar xalan.jar -OUT ../gli/0.6.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.1'
 java -jar xalan.jar -OUT ../gli/0.6.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.1'
 java -jar xalan.jar -OUT ../gli/0.6.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.1'
+
+java -jar xalan.jar -OUT ../gli/0.6.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
+java -jar xalan.jar -OUT ../gli/0.6.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
+java -jar xalan.jar -OUT ../gli/0.6.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.6.0'
+
+java -jar xalan.jar -OUT ../gli/0.5.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
+java -jar xalan.jar -OUT ../gli/0.5.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
+java -jar xalan.jar -OUT ../gli/0.5.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.1'
+
+java -jar xalan.jar -OUT ../gli/0.5.0/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
+java -jar xalan.jar -OUT ../gli/0.5.0/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
+java -jar xalan.jar -OUT ../gli/0.5.0/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.5.0'
+
+java -jar xalan.jar -OUT ../gli/0.4.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
+java -jar xalan.jar -OUT ../gli/0.4.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
+java -jar xalan.jar -OUT ../gli/0.4.1/code.html     -IN ../gli/source/data.xml   -XSL ./code.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.4.1'
+
