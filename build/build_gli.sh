25895dd3abcfe0dd8c9a994874fc68b6f6095030
Christophe Riccio <christophe.riccio@unity3d.com>
2016-11-12 23:11:58 +0100

    GLI 0.8.2 released

--- build/build_gli.sh
+++ build/build_gli.sh
@@ -1,4 +1,4 @@
-java -jar xalan.jar -OUT ../gli/gli.atom            -IN ../gli/source/data.xml   -XSL ./atom.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM SERVE atom
+java -jar xalan.jar -OUT ../gli/0.8.2/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.8.2'
 
 java -jar xalan.jar -OUT ../gli/0.8.1/index.html    -IN ../gli/source/data.xml   -XSL ./index.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.8.1'
 java -jar xalan.jar -OUT ../gli/0.8.1/updates.html  -IN ../gli/source/data.xml   -XSL ./updates.xsl	-PARAM DIRECTORY ../gli/source/ -PARAM VERSION '0.8.1'
